2c40d2f80b48b41d624daa9117f48de08110b60f
Ganesh Kumar <275278596+GaneshKumar-Tech@users.noreply.github.com>
2026-06-03 16:32:32 +0530

    Added the Product Flavors

--- projects/github-actions-demo/app/build.gradle.kts
+++ projects/github-actions-demo/app/build.gradle.kts
@@ -27,6 +27,47 @@ android {
             )
         }
     }
+
+    flavorDimensions += "environment"
+
+    productFlavors {
+
+        create("dev") {
+            dimension = "environment"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+            versionCode = 1
+            versionName = "1.0"
+            buildConfigField("String", "BASE_URL", "\"https://dev.example.com\"")
+            //buildConfigField("String", "API_KEY", "dev-api-key")
+            buildConfigField("String", "ENVIRONMENT", "\"DEV\"")
+            resValue("string", "app_name", "GitHub-Actions-Demo-DEV")
+        }
+
+        create("qa") {
+            dimension = "environment"
+            applicationIdSuffix = ".qa"
+            versionNameSuffix = "-qa"
+            versionCode = 1
+            versionName = "1.0"
+            buildConfigField("String", "BASE_URL", "\"https://qa.example.com\"")
+            //buildConfigField("String", "API_KEY", "qa-api-key")
+            buildConfigField("String", "ENVIRONMENT", "\"QA\"")
+            resValue("string", "app_name", "GitHub-Actions-Demo-QA")
+        }
+
+        create("prod") {
+            dimension = "environment"
+            versionCode = 1
+            versionName = "1.0"
+            buildConfigField("String", "BASE_URL", "\"https://prod.example.com\"")
+            //buildConfigField("String", "API_KEY", "prod-api-key")
+            buildConfigField("String", "ENVIRONMENT", "\"PROD\"")
+            resValue("string", "app_name", "GitHub-Actions-Demo")
+        }
+
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,6 +77,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
--- projects/github-actions-demo/app/src/main/java/com/example/githubactionsdemo/MainActivity.kt
+++ projects/github-actions-demo/app/src/main/java/com/example/githubactionsdemo/MainActivity.kt
@@ -4,44 +4,96 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.githubactionsdemo.ui.theme.GithubActionsDemoTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
-            GithubActionsDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            GithubActionsDemoTheme(darkTheme = false) {
+
+                MainScreen()
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+
+    Scaffold(
+
+        topBar = {
+
+            TopAppBar(
+
+                title = {
+
+                    Text(text = "GitHub Actions Demo")
+
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+
+                    containerColor = Color(0xFF6F52B5),
+                    titleContentColor = Color.White
+
+                )
+            )
+        }
+
+    ) { innerPadding ->
+
+        Box(
+
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+
+            contentAlignment = Alignment.Center
+
+        ) {
+
+            Column {
+                Text(
+                    text = "Hello ${BuildConfig.ENVIRONMENT}!"
+                )
+                Text(
+                    text = "URL: ${BuildConfig.BASE_URL}"
+                )
+            }
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GithubActionsDemoTheme {
-        Greeting("Android")
+fun MainScreenPreview() {
+
+    GithubActionsDemoTheme(darkTheme = false) {
+
+        MainScreen()
+
     }
 }
